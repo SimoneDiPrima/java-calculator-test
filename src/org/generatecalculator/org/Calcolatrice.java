0b99b2ee35069efbeb15fe01d3448bceb468f188
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-12-07 17:52:08 +0100

    first commit

---
+++ src/org/generatecalculator/org/Calcolatrice.java
@@ -0,0 +1,52 @@
+package org.generatecalculator.org;
+
+public class Calcolatrice {
+	private float num1;
+	private float num2;
+	
+	public Calcolatrice(int num1,int num2) {
+		setNum1(num1);
+		setNum2(num2);
+	}
+	public float getNum1() {
+		return num1;
+	}
+	public void setNum1(int num1) {
+		this.num1 = num1;
+	}
+	public float getNum2() {
+		return num2;
+	}
+	public void setNum2(int num2) {
+		this.num2 = num2;
+	}
+	public float add(float num1,float num2) {
+		float numAdd = 0;
+		return numAdd = num1 + num2;
+	}
+	public float substract(float num1,float num2) {
+		float numSubstract = 0;
+		if(num1 >= num2) {
+			return numSubstract = num1 - num2;
+		}
+		else numSubstract = num2 - num1;
+		return numSubstract;
+	}
+	public float multiply(float num1,float num2) {
+		float numMultiply = 0;
+		if(num1 > 0) {
+			return numMultiply = num1 * num2;
+		}
+	
+		return numMultiply;
+	}
+	public float divide(float num1,float num2) {
+		float numMultiply = 0;
+		if(num1 > 0) {
+			return numMultiply = num1 * num2;
+		}
+	
+		return numMultiply;
+	}
+	
+}
